69e6b88491447dd291ab6d26598f9cccb4110940
Ryan Coulter <ryan.coulter0@gmail.com>
2013-02-01 10:36:04 -0500

    About page layout
    Added javascript to force the footer to the bottom of the page.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+ $(document).ready(function () {
+     $(window).resize(function(){//Keeps the footer at the bottom of the page by setting the main-container's height to be the entire screen minus the header and footer.
+         var hHeight = $(".header-container").height();
+         var fHeight = $(".footer-container").height();
+         var borders = 20;
+         $(".main-container").height(($(window).height() - hHeight) - fHeight - borders);
+     });
+     $(window).trigger("resize");
+});
